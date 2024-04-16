717c4cf85a02923ad5a269a76b8155ff33730c61
zhaotinghao <tinghao_zhao@brown.edu>
2024-04-16 05:12:13 -0400

    implement a concurrent proxy

--- proxy.c
+++ proxy.c
@@ -1,10 +1,14 @@
 #include <stdio.h>
-#include "tiny/csapp.h"
+#include "csapp.h"
+#include "sbuf.h"
+
 
 /* Recommended max cache and object sizes */
 #define MAX_CACHE_SIZE 1049000
 #define MAX_OBJECT_SIZE 102400
 #define WEB_PREFIX "http://"
+#define NTHREADS  4
+#define SBUFSIZE  16
 
 /* You won't lose style points for including this long line in your code */
 static const char *user_agent_hdr = "User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:10.0.3) Gecko/20120305 Firefox/10.0.3\r\n";
@@ -19,11 +23,16 @@ void replaceHTTPVersion(char *);
 
 void parseLine(char *, char *, char *, char *, char *, char *, char *);
 
+void *thread(void *vargp);
+
+sbuf_t sbuf; /* Shared buffer of connected descriptors */
+
 int main(int argc, char **argv) {
     int listenfd, connfd;
     char hostname[MAXLINE], port[MAXLINE];
     socklen_t clientlen;
     struct sockaddr_storage clientaddr;
+    pthread_t tid;
 
     /* Check command line args */
     if (argc != 2) {
@@ -32,14 +41,28 @@ int main(int argc, char **argv) {
     }
 
     listenfd = Open_listenfd(argv[1]);
+
+    sbuf_init(&sbuf, SBUFSIZE);
+    for (int i = 0; i < NTHREADS; i++)  /* Create worker threads */
+        Pthread_create(&tid, NULL, thread, NULL);
+
     while (1) {
         clientlen = sizeof(clientaddr);
         connfd = Accept(listenfd, (SA *) &clientaddr, &clientlen); //line:netp:tiny:accept
         Getnameinfo((SA *) &clientaddr, clientlen, hostname, MAXLINE,
                     port, MAXLINE, 0);
         printf("Accepted connection from (%s, %s)\n", hostname, port);
-        handleRequest(connfd);                                             //line:netp:tiny:doit
-        Close(connfd);                                            //line:netp:tiny:close
+        sbuf_insert(&sbuf, connfd); /* Insert connfd in buffer */
+    }
+}
+
+void *thread(void *vargp) {
+    //threads should run in detached mode to avoid memory leaks.
+    Pthread_detach(pthread_self());
+    while (1) {
+        int connfd = sbuf_remove(&sbuf); /* Remove connfd from buffer */
+        handleRequest(connfd);              /* Service client */
+        Close(connfd);
     }
 }
 
@@ -101,7 +124,6 @@ void handleRequest(int fd) {
     /** step4: read the response from tiny and send it to the client */
     printf("---prepare to get the response---- \n");
     char tinyResponse[MAXLINE];
-    char *tinyResponseP = tinyResponse;
 
     int n;
     while ((n = Rio_readnb(&rioTiny, tinyResponse, MAXLINE)) != 0) {
